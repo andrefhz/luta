48238a28a77ba2a43aed74aad829255207a65f95
André Cavalcanti <andre.cavalcanti.simoes@gmail.com>
2023-05-22 23:53:06 -0300

    Criação de classes básicas e testando nos log

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let char = new Sorcerer('Andre');
+
+console.log(char.name);
+console.log(char.life);
+console.log(char.attack);
+
+
+let monster = new Troll();
+
+console.log(monster.name);
+console.log(monster.attack);
+console.log(monster.life);
